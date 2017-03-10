e430293008c14155bdad41b1705804ebc14d5c6f
MarvenGong <1637077309@qq.com>
2017-03-10 15:40:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,264 @@
+# etools
+### 安装和使用
++ 使用npm安装：`npm install -s etools`
+
+  通过es6模块引入，如`import _ from "etools";console.log(_.extend(true,{},{"age":23}));`
++ 下载并在页面引入etools.js
+
+    直接调用ETool对象中得方法，如`console.log(ETools.extend(true,{},{"age":23}));`
+***
+### 详细文档
+#### 对象相关
++ `ETools.extend(deep,obj1,obj2)`
+      
+    作用：类似于jquery.extend方法，合并json对象，并将合并后的对象保存到obj1中作为返回值
+      
+    参数：
+        1. deep,是否深度复制，设为true，会复制对象中嵌套的对象。
+        2. obj1,obj2,要合并的对象
+      
+    案例：`ETools.extend(true,{"name":"zhangsan",gender:"male"},{"age":12})`
+            输出结果`{"name":"zhangsan",gender:"male","age":12}`
+      
+    说明，我们可以将obj1设为空{}，用这个方法克隆一个对象，解决引用类型赋值的问题
+#### url请求相关
++ `ETools.deparam(str)`
+
+    将jquery序列化表单之后得到的参数字符串转换成标准的json对象
+
+    str,即要格式化的字符串
+
+    ETools.deparam("aaa=bbb&ccc=ddd")输出结果{aaa:bbb,ccc:ddd}
+
+    <a href="http://www.gongminghua.site/ETools/formDeparam">查看演示>></a>
++ `ETools.urlParamToObj(url)`
+
+    将连接中的get请求参数转换成json对象
+
+    url:链接
+
+    `ETools.deparam("www.baidu.com?aaa=bbb&ccc=ddd")`输出结果`{aaa:bbb,ccc:ddd}`
+
+#### 事件
++ `ETools.stopPropagation(event)`
+
+    阻止事件向上冒泡，点击某元素不会触发父元素的事件
+        
+    event:事件对象
+
+    <code>ETools.stopPropagation(event)</code>
+
+#### 时间日期相关
++ `ETools.datetime.compareDate(starate,endDate)`
+
+    比较两个日期的大小
+
+    日期格式"yyyy-mm-dd",如果返回值为-1，则前者大于后者，为0两者相等，为1，后者大于前者
+
+    <code>ETools.datetime.compareDate("2015-12-11","2015-12-12")</code>
++ `ETools.datetime.compareTime(startTime,endTime)`
+
+    比较两个时间的大小
+
+    日期格式"yyyy-mm-dd h:mi:s",如果返回值为-1，则前者大于后者，为0两者相等，为1，后者大于前者
+
+    `ETools.datetime.compareDate("2015-12-11 12:10:00","2015-12-12 12:10:00")`
++ `ETools.datetime.arriveTimerFormat(s)`
+
+    将秒数转换成对应的多少小时，多少分多少秒（用于倒计时的显示）
+
+    s:要计算的秒数
++ `ETools.datetime.arriveTimerFormat(52010)->[0, 14, 26, 50, "14:26:50"]`
++ `ETools.datetime.format(date,formatStr)`
+
+    将Date类型的时间转换成指定格式的字符串
+
+    <p>date:日期类型的对象（如：new Data()）</p>formatStr：想要的时间格式如："yyyy-mm-dd h:m:s 星期w"
+
+    ETools.datetime.Format(new Date(),"yyyy-MM-dd h:m:s 星期w")->"2015-16-24 15:16:15 星期四"
++ `ETools.datetime.parse(timeStr)`
+
+    将时间字符串转换成对应的时间戳
+
+    <p>timeStr:时间字符串（如："2015-16-24 15:16:15"）</p>
+
+    `ETools.datetime.parse("2015-16-24 15:16:15")->1461482175`
+
+####html格式操作
++ `ETools.html.htmlEncode(str)`
+
+    把html中的常见符号转换成特殊符号码，如"<"转换成<pre>"&amp;lt;"</pre>
+
+    参数str，要转换的html代码
++ `ETools.html.htmlDecode(str)`
+
+    把html代码转换成html元素
+
+    参数str，要转换的html字符串
+
+#### 页面操作
++ `ETools.page.addFavorite(url,title)`
+
+    将页面添加到浏览器收藏夹
+
+    url:要添加的地址信息。title:在收藏夹中的名字
+
+    ETools.page.addFavorite("www.baidu.com","百度")
++ `ETools.page.addHome(url)`
+
+    将页面设为浏览器的首页
+
+    url:要添加的地址信息。
+
+    ETools.page.addFavorite("www.baidu.com")
+#### 动态加载
++ `ETools.loadStyle(styleUrl)`
+
+    动态加载样式表
+
+    styleUrl：要加载的样式文件的地址
+
+    ETools.loadStyle("css/style.css")
+
+#### 浏览器信息获取
++ `ETools.getExplorerInfo()`
+            
+    返回浏览器的名称以及版本信息{browerName:"google",version:"3.10.101.1"}
+
+#### 字符串操作
++ ETools.string.getStrLength(str)
+
+    获取字符串长度，中文字符按两个长度计算
+
+    str:要计算的字符串
+
+    ETools.string.getStrLength("ilove你")->7
++ ETools.string.trim(str)
+
+    去掉字符串左右的空格
+
+    str:要去空格的字符串
+
+    ETools.string.getStrLength(" aaa  ")->"aaa"
+
+#### 表单验证
++ `ETools.vertify.isURL(str)`
+
+    验证url地址
+
+    ETools.vertify.isURL("www.baidu.com")->true
++ `ETools.vertify.isEmpty(str)`
+
+    验证是否为空
+
+    ETools.vertify.isEmpty(" ")->true
++ `ETools.vertify.isDigit(str)`
+        
+    验证是否是数字
+        
+    ETools.vertify.isDigit("2.0")->false
++ `ETools.vertify.isTelephone(str)`
+        
+    验证固定电话
+
+    ETools.vertify.isTelephone("023-55813950")->true
++ `ETools.vertify.isMobile(str)`
+        
+    验证手机号码
+        
+    ETools.vertify.isMobile("15696544221")->true
++ `ETools.vertify.isQQ(str)`
+        
+    验证QQ号
+        
+    ETools.vertify.isQQ("1634251421")->true
++ `ETools.vertify.isEmail(str)`
+        
+    验证邮箱地址
+        
+    ETools.vertify.isEmail("2542152@qq.com")->true
++ `ETools.vertify.isIDCard(str)`
+        
+    验证身份证号码
+        
+    ETools.vertify.isIDCard("500235199008205570")->true
++ ` ETools.vertify.isPlusDigit(str)`
+        
+    验证是否是无符号正整数
+        
+    ETools.vertify.isPlusDigit("52")->true
++ `ETools.vertify.isChinese(str)`
+        
+    验证中文字符
+        
+    ETools.vertify.isChinese("我爱你")->true
++ ` ETools.vertify.isDate(str)`
+        
+    验证日期
+        
+    ETools.vertify.isDate("2015-12-12")->true
++ `ETools.vertify.isPostalCode(str)`
+        
+    验证邮政编码
+        
+    ETools.vertify.isPostalCode("400521")->true
++ `ETools.vertify.isRegisterUserName(str)`
+        
+    验证登录名，只能输入5-20个以字母开头、可带数字、“_”、“.”的字串
+        
+    ETools.vertify.isRegisterUserName("gong163")->true
++ ` ETools.vertify.isTrueName(str)`
+        
+    校验用户姓名：只能输入1-30个以字母开头的字串
+        
+    ETools.vertify.isTrueName("龚明华")->true
++ `ETools.vertify.isPassword(str)`
+        
+    校验密码：只能输入6-20个字母、数字、下划线
+        
+    ETools.vertify.isPassword("gong1632542142")->true
+
+#### Cookie操作
++ `ETools.cookie.setCookie(name, value, Hours)`
+        
+    设置Cookie值
+        
+    name:cookie的key，value:cookie的value，Hours：Cookie的超时时间
+        
+    ETools.cookie.setCookie("username", "xiaoming", 0.5)
++ `ETools.cookie.getCookie(name)`
+        
+    获取Cookie值
+        
+    name:cookie的key
+        
+    ETools.cookie.getCookie("username")->"xiaoming"
++ `ETools.cookie.delCookie(name)`
+        
+    删除Cookie
+        
+    name:cookie的key
+        
+    ETools.cookie.delCookie("username")
+
+#### html格式操作
++ `ETools.openWindow(url,windowName,width,height)`
+        
+    打开一个弹窗窗口
+        
+    url打开的链接，windowName窗口的title，width窗口宽度，height窗口高度
+        
+    ETools.openWindow("www.baidu.com","百度",500,600)
+
+#### jquery插件
++ `checkAll()`
+        
+    表格的行的全选和反全选
+        
+    `$(".checkAll").checkAll();`
+        
+    需要给复选框绑定checkAll()事件。为其加上data-table属性，属性值指向要执行全选的table的id。
+        
+    如果有多个复选框共同作用于同一个表格，给这些复选框加上相同的data-table属性值即可
+        
+    <p><a href="http://www.gongminghua.site/ETools/checkAll">查看演示>></a> </p>
---
+++ index.js
@@ -0,0 +1,629 @@
+/*
+ * Author Marvengong of etekcity
+ */
+export default class ETools {
+
+}
+const digitTest = /^\d+$/;
+const keyBreaker = /([^\[\]]+)|(\[\])/g;
+const paramTest = /([^?#]*)(#.*)?$/, prep = function (str) {
+    return decodeURIComponent(str.replace(/\+/g, ' '));
+};
+/**
+ * 复制或者合并对象
+ * @param deep 是否深度拷贝
+ * @param target 合并后的对象
+ * @param options 要合并的对象
+ * @returns {*}
+ */
+ETools.extend = (deep, target, options) => {
+    let copyIsArray;
+    let toString = Object.prototype.toString;
+    let hasOwn = Object.prototype.hasOwnProperty;
+
+    const class2type = {
+        '[object Boolean]': 'boolean',
+        '[object Number]': 'number',
+        '[object String]': 'string',
+        '[object Function]': 'function',
+        '[object Array]': 'array',
+        '[object Date]': 'date',
+        '[object RegExp]': 'regExp',
+        '[object Object]': 'object'
+    };
+
+    let type = function (obj) {
+        return obj == null ? String(obj) : class2type[toString.call(obj)] || "object";
+    };
+
+    let isWindow = function (obj) {
+        return obj && typeof obj === "object" && "setInterval" in obj;
+    };
+
+    let isArray = Array.isArray || function (obj) {
+            return type(obj) === "array";
+        };
+
+    let isPlainObject = function (obj) {
+        if (!obj || type(obj) !== "object" || obj.nodeType || isWindow(obj)) {
+            return false;
+        }
+
+        if (obj.constructor && !hasOwn.call(obj, "constructor")
+            && !hasOwn.call(obj.constructor.prototype, "isPrototypeOf")) {
+            return false;
+        }
+
+        var key;
+        for (key in obj) {
+        }
+
+        return key === undefined || hasOwn.call(obj, key);
+    };
+    for (name in options) {
+        let src = target[name];
+        let copy = options[name];
+
+        if (target === copy) {
+            continue;
+        }
+
+        if (deep && copy
+            && (isPlainObject(copy) || (copyIsArray = isArray(copy)))) {
+            if (copyIsArray) {
+                copyIsArray = false;
+                clone = src && isArray(src) ? src : [];
+
+            } else {
+                clone = src && isPlainObject(src) ? src : {};
+            }
+
+            target[name] = extend(deep, clone, copy);
+        } else if (copy !== undefined) {
+            target[name] = copy;
+        }
+    }
+
+    return target;
+};
+/**
+ * 将jquery序列化表单后得到的字符串转换成标准的json对象
+ * @param params--jquery序列化后的字符串
+ * @returns {{}}
+ */
+ETools.deparam = function (params) {
+    let data = {}, pairs, lastPart;
+    if (params && paramTest.test(params)) {
+        let pairs = params.split('&');
+        $.each(pairs, function (index, pair) {
+            let parts = pair.split('='), key = prep(parts.shift()), value = prep(parts.join('=')), current = data;
+            if (key) {
+                parts = key.match(keyBreaker);
+                for (let j = 0, l = parts.length - 1; j < l; j++) {
+                    if (!current[parts[j]]) {
+                        current[parts[j]] = digitTest.test(parts[j + 1]) || parts[j + 1] === '[]' ? [] : {};
+                    }
+                    current = current[parts[j]];
+                }
+                lastPart = parts.pop();
+                if (lastPart === '[]') {
+                    current.push(value);
+                } else {
+                    current[lastPart] = value;
+                }
+            }
+        });
+    }
+    return data;
+};
+/**
+ * 将url中的get参数转换成标准json对象
+ * @param u
+ * @returns {{}}
+ */
+ETools.urlParamToObj = function (u) {
+    let sear = u.slice(u.indexOf("?") + 1).split("&"), p = {};
+    for (let i = 0, j = sear.length; j > i; i++) {
+        let s = sear[i].split("=");
+        p[s[0]] = s[1];
+    }
+    return p;
+};
+/**
+ * 阻止事件向上冒泡
+ * @param e
+ */
+ETools.stopPropagation = function (e) {
+    //判断是否是IE浏览器，不是就用e.stopPropagation否则用 e.cancelBubble=true阻止事件冒泡
+    if (e.stopPropagation)
+        e.stopPropagation();
+    else
+        e.cancelBubble = true;
+};
+
+/**
+ *html转移和还原
+ */
+ETools.html = {
+    htmlEncode: function (text) {
+        return text.replace(/&/g, '&amp').replace(/\"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    },
+    htmlDecode: function (text) {
+        return text.replace(/&amp;/g, '&').replace(/&quot;/g, '\"').replace(
+            /&lt;/g, '<').replace(/&gt;/g, '>');
+    }
+};
+/**
+ * 页面操作
+ * @type {{addFavorite: Function, addHome: Function}}
+ */
+ETools.page = {
+    /**
+     * 加入收藏夹
+     * @param sURL
+     * @param sTitle
+     */
+    addFavorite: function (sURL, sTitle) {
+        try {
+            window.external.addFavorite(sURL, sTitle);
+        } catch (e) {
+            try {
+                window.sidebar.addPanel(sTitle, sURL, "");
+            } catch (e) {
+                alert("加入收藏失败，请使用Ctrl+D进行添加");
+            }
+        }
+    },
+    /**
+     * 设为首页
+     */
+    addHome: function (url) {
+        if (document.all) {
+            document.body.style.behavior = 'url(#default#homepage)';
+            document.body.setHomePage(url);
+        } else if (window.sidebar) {
+            if (window.netscape) {
+                try {
+                    netscape.security.PrivilegeManager
+                        .enablePrivilege("UniversalXPConnect");
+                } catch (e) {
+                    alert("该操作被浏览器拒绝，如果想启用该功能，请在地址栏内输入 about:config,然后将项 signed.applets.codebase_principal_support 值该为true");
+                }
+            }
+            let prefs = Components.classes['@mozilla.org/preferences-service;1']
+                .getService(Components.interfaces.nsIPrefBranch);
+            prefs.setCharPref('browser.startup.homepage', url);
+        }
+    }
+};
+/**
+ * 动态加载样式文件
+ * @param url--样式的地址
+ * @constructor
+ */
+ETools.loadStyle = function (url) {
+    try {
+        document.createStyleSheet(url);
+    } catch (e) {
+        let cssLink = document.createElement('link');
+        cssLink.rel = 'stylesheet';
+        cssLink.type = 'text/css';
+        cssLink.href = url;
+        let head = document.getElementsByTagName('head')[0];
+        head.appendChild(cssLink);
+    }
+}
+/**
+ * 返回浏览器信息，
+ * 返回值{type:浏览器类型,version:浏览器版本}
+ * @returns {*}
+ */
+ETools.getExplorerInfo = function () {
+    let explorer = window.navigator.userAgent.toLowerCase();
+    // ie
+    if (explorer.indexOf("msie") >= 0) {
+        let ver = explorer.match(/msie ([\d.]+)/)[1];
+        return {
+            type: "IE",
+            version: ver
+        };
+    }
+    // firefox
+    else if (explorer.indexOf("firefox") >= 0) {
+        let ver = explorer.match(/firefox\/([\d.]+)/)[1];
+        return {
+            type: "Firefox",
+            version: ver
+        };
+    }
+    // Edge
+    else if (explorer.indexOf("edge") >= 0) {
+        let ver = explorer.match(/edge\/([\d.]+)/)[1];
+        return {
+            type: "Edge",
+            version: ver
+        };
+    }
+    // Chrome
+    else if (explorer.indexOf("chrome") >= 0) {
+        let ver = explorer.match(/chrome\/([\d.]+)/)[1];
+        return {
+            type: "Chrome",
+            version: ver
+        };
+    }
+
+    // Opera
+    else if (explorer.indexOf("opera") >= 0) {
+        let ver = explorer.match(/opera.([\d.]+)/)[1];
+        return {
+            type: "Opera",
+            version: ver
+        };
+    }// Safari
+    else if (explorer.indexOf("Safari") >= 0) {
+        let ver = explorer.match(/version\/([\d.]+)/)[1];
+        return {
+            type: "Safari",
+            version: ver
+        };
+    }
+}
+/**
+ * 字符串操作
+ * @param str
+ * @returns {string|XML}
+ */
+ETools.string = {
+    //获取字符串长度，中文字符计算为两个长度
+    getStrLength: function (str) {
+        let strlen = 0;
+        for (let i = 0; i < str.length; i++) {
+            if (str.charCodeAt(i) > 255) //如果是汉字，则字符串长度加2
+                strlen += 2;
+            else
+                strlen++;
+        }
+        return strlen;
+    },
+    //去掉字符串前后空格
+    trim: function (str) {
+        return str.replace(/^(\s|\u00A0)+/, '').replace(/(\s|\u00A0)+$/, '');
+    }
+
+}
+/**
+ * 正则验证
+ */
+ETools.vertify = {
+    //判断是否是一个标准url
+    isURL: function (strUrl) {
+        let regular = /^\b(((https?|ftp):\/\/)?[-a-z0-9]+(\.[-a-z0-9]+)*\.(?:com|edu|gov|int|mil|net|org|biz|info|name|museum|asia|coop|aero|[a-z][a-z]|((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d))\b(\/[-a-z0-9_:\@&?=+,.!\/~%\$]*)?)$/i;
+        if (regular.test(strUrl)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    },
+    isEmpty: function (str) {
+        if (str == null || typeof str == "undefined" || ETools.string.trim(str) == "") {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    //判断是否是数字
+    isDigit: function (value) {
+        let patrn = /^[0-9]*$/;
+        if (patrn.exec(value) == null || value == "") {
+            return false;
+        } else {
+            return true;
+        }
+    },
+    //判断是否是固定电话
+    isTelephone: function (phone) {
+        let phone_reg = new RegExp(/^([+]{0,1}\d{3,4}|\d{3,4}-)?\d{7,8}$/);
+        if (!phone_reg.test(phone)) {
+            return false;
+        }
+        return true;
+    },
+    isMobile: function (mobile) {
+        let mobile_reg = new RegExp(/^0{0,1}1[0-9]{10}$/);
+        if (!mobile_reg.test(mobile)) {
+            return false;
+        }
+        return true;
+    },
+    //QQ号码从10000开始
+    isQQ: function (qq) {
+        let qq_reg = new RegExp(/^[1-9][0-9]{4,}$/);
+        if (!qq_reg.test(qq)) {
+            return false;
+        }
+        return true;
+    },
+    //验证是否是邮箱
+    isEmail: function (email) {
+        let email_reg = new RegExp(/^\w+([-+.]\w+)*@\w+([-.]\w+)*.\w+([-.]\w+)*$/);
+        if (!email_reg.test(email)) {
+            return false;
+        }
+        return true;
+    },
+    //身份证
+    isIDCard: function (str) {
+        let IDCardReg = new RegExp(/(^\d{15}$)|(^\d{17}([0-9]|X)$)/);
+        if (!IDCardReg.test(str)) {
+            return false;
+        }
+        return true;
+    },
+    //不带符号正整数
+    isPlusDigit: function (digit) {
+        let plusDigit_reg = new RegExp(/^\d+$/);
+        if (!plusDigit_reg.test(digit)) {
+            return false;
+        }
+        return true;
+    },
+    //中文字符
+    isChinese: function (str) {
+        if (str.length != 0) {
+            reg = /^[\u0391-\uFFE5]+$/;
+            if (!reg.test(str)) {
+                return false;
+            }
+        }
+        return true;
+    },
+    //日期格式的字符串
+    isDate: function (datestr) {
+        let result = datestr.match(/((^((1[8-9]\d{2})|([2-9]\d{3}))(-)(10|12|0?[13578])(-)(3[01]|[12][0-9]|0?[1-9])$)|(^((1[8-9]\d{2})|([2-9]\d{3}))(-)(11|0?[469])(-)(30|[12][0-9]|0?[1-9])$)|(^((1[8-9]\d{2})|([2-9]\d{3}))(-)(0?2)(-)(2[0-8]|1[0-9]|0?[1-9])$)|(^([2468][048]00)(-)(0?2)(-)(29)$)|(^([3579][26]00)(-)(0?2)(-)(29)$)|(^([1][89][0][48])(-)(0?2)(-)(29)$)|(^([2-9][0-9][0][48])(-)(0?2)(-)(29)$)|(^([1][89][2468][048])(-)(0?2)(-)(29)$)|(^([2-9][0-9][2468][048])(-)(0?2)(-)(29)$)|(^([1][89][13579][26])(-)(0?2)(-)(29)$)|(^([2-9][0-9][13579][26])(-)(0?2)(-)(29)$))/);
+        if (result == null) {
+            return false;
+        }
+        return true;
+    },
+    //校验邮政编码
+    isPostalCode: function (s) {
+        //let patrn=/^[a-zA-Z0-9]{3,12}$/;
+        let patrn = /^[a-zA-Z0-9 ]{3,12}$/;
+        if (!patrn.exec(s)) {
+            return false
+        }
+        return true
+    },
+    //校验登录名：只能输入5-20个以字母开头、可带数字、“_”、“.”的字串
+    isRegisterUserName: function (s) {
+        let patrn = /^[a-zA-Z]{1}([a-zA-Z0-9]|[._]){4,19}$/;
+        if (!patrn.exec(s)) return false
+        return true
+    },
+    //校验用户姓名：只能输入1-30个以字母开头的字串
+    isTrueName: function (s) {
+        let patrn = /^[a-zA-Z]{1,30}$/;
+        if (!patrn.exec(s)) return false
+        return true
+    },
+    //校验密码：只能输入6-20个字母、数字、下划线
+    isPassword: function (s) {
+        let patrn = /^(\w){6,20}$/;
+        if (!patrn.exec(s)) return false
+        return true
+    }
+}
+/**
+ * cookie操作
+ */
+ETools.cookie = {
+    delCookie: function (name) {
+        let exp = new Date();
+        exp.setTime(exp.getTime() - 1);
+        let cval = getCookie(name);
+        if (cval != null)
+            document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+    },
+    setCookie: function (name, value, Hours) {
+        let d = new Date();
+        let offset = 8;
+        let utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+        let nd = utc + (3600000 * offset);
+        let exp = new Date(nd);
+        exp.setTime(exp.getTime() + Hours * 60 * 60 * 1000);
+        document.cookie = name + "=" + escape(value) + ";path=/;expires="
+            + exp.toGMTString() + ";domain=sicd.com;";
+    },
+    getCookie: function (name) {
+        let arr = document.cookie
+            .match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+        if (arr != null)
+            return unescape(arr[2]);
+        return null;
+    }
+};
+/**
+ * 打开一个新窗口
+ * @param url打开的链接
+ * @param windowName窗口的title
+ * @param width窗口宽度
+ * @param height窗口高度
+ */
+ETools.openWindow = function (url, windowName, width, height) {
+    let x = parseInt(screen.width / 2.0) - (width / 2.0);
+    let y = parseInt(screen.height / 2.0) - (height / 2.0);
+    let isMSIE = (navigator.appName == "Microsoft Internet Explorer");
+    if (isMSIE) {
+        let p = "resizable=1,location=no,scrollbars=no,width=";
+        p = p + width;
+        p = p + ",height=";
+        p = p + height;
+        p = p + ",left=";
+        p = p + x;
+        p = p + ",top=";
+        p = p + y;
+        retval = window.open(url, windowName, p);
+    } else {
+        let win = window.open(url, "ZyiisPopup", "top=" + y + ",left=" + x + ",scrollbars=" + scrollbars + ",dialog=yes,modal=yes,width=" + width + ",height=" + height + ",resizable=no");
+        eval("try { win.resizeTo(width, height); } catch(e) { }");
+        win.focus();
+    }
+};
+/**
+ * 时间日期相关函数
+ * @type {{}}
+ */
+ETools.datetime = {
+    /**
+     *比较日期大小,格式yyyy-MM-dd
+     *小于0前者大于后者,大于0前者小于后者,等于0两者相等
+     * @param startTime
+     * @param endTime
+     * @returns {number}
+     */
+    compareDate: function (startTime, endTime) {
+        let startArr = startTime.split("-");
+        let starttime = new Date(startArr[0], startArr[1], startArr[2]);
+        let starttimes = starttime.getTime();
+
+        let endArr = endTime.split("-");
+        let endtime = new Date(endArr[0], endArr[1], endArr[2]);
+        let endtimes = endtime.getTime();
+
+        if (starttimes > endtimes) {
+            return -1;
+        }
+        else if (starttimes == endtimes) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    },
+
+    /**
+     * 比较时间的大小
+     * 时间格式yyyy-MM-dd h:m:s
+     *    如：let beginTime = "2009-09-21 00:00:00";let endTime = "2009-09-21 00:00:01";
+     */
+    compareTime: function (beginTime, endTime) {
+
+        let beginTimes = beginTime.substring(0, 10).split('-');
+        let endTimes = endTime.substring(0, 10).split('-');
+
+        beginTime = beginTimes[1] + '-' + beginTimes[2] + '-' + beginTimes[0] + ' ' + beginTime.substring(10, 19);
+        endTime = endTimes[1] + '-' + endTimes[2] + '-' + endTimes[0] + ' ' + endTime.substring(10, 19);
+        /*
+         alert(beginTime + "aaa" + endTime);
+         alert(Date.parse(endTime));
+         alert(Date.parse(beginTime));*/
+        let a = (Date.parse(endTime) - Date.parse(beginTime)) / 3600 / 1000;
+        if (a < 0) {
+            return -1;
+        } else if (a > 0) {
+            return 1;
+        } else if (a == 0) {
+            return 0;
+        } else {
+            return 'exception'
+        }
+    },
+    /**
+     * 将秒数转换成对应的多少小时，多少分多少秒（用于倒计时的显示）
+     * @param s
+     * @returns {*[]}
+     */
+    arriveTimerFormat: function (s) {
+        let t, hour, min, sec, day;
+        if (s > -1) {
+            hour = Math.floor(s / 3600);
+            min = Math.floor(s / 60) % 60;
+            sec = s % 60;
+            day = parseInt(hour / 24);
+            if (day > 0) {
+                hour = hour - 24 * day;
+                t = day + "day " + hour + ":";
+            } else t = hour + ":";
+            if (min < 10) {
+                t += "0";
+            }
+            t += min + ":";
+            if (sec < 10) {
+                t += "0";
+            }
+            t += sec;
+        }
+        return [day, hour, min, sec, t];
+    },
+    /**
+     *
+     * @param data 日期类型的对象（如：new Date()）
+     * @param formatStr 要格式化的日期时间格式（如：“yyyy-MM-dd h:m:s”）
+     * @returns {*}
+     */
+    format: function (date, formatStr) {
+        let str = formatStr;
+        let Week = ['日', '一', '二', '三', '四', '五', '六'];
+        str = str.replace(/yyyy|YYYY/, date.getFullYear());
+        str = str.replace(/yy|YY/, (date.getYear() % 100) > 9 ? (date.getYear() % 100).toString() : '0' + (date.getYear() % 100));
+        str = str.replace(/MM/, (date.getMonth() + 1) > 9 ? (date.getMonth() + 1).toString() : '0' + (date.getMonth() + 1));
+        str = str.replace(/M/g, (date.getMonth() + 1));
+        str = str.replace(/w|W/g, Week[date.getDay()]);
+        str = str.replace(/dd|DD/, date.getDate() > 9 ? date.getDate().toString() : '0' + date.getDate());
+        str = str.replace(/d|D/g, date.getDate());
+        str = str.replace(/hh|HH/, date.getHours() > 9 ? date.getHours().toString() : '0' + date.getHours());
+        str = str.replace(/h|H/g, date.getHours());
+        str = str.replace(/mm/, date.getMinutes() > 9 ? date.getMinutes().toString() : '0' + date.getMinutes());
+        str = str.replace(/m/g, date.getMinutes());
+        str = str.replace(/ss|SS/, date.getSeconds() > 9 ? date.getSeconds().toString() : '0' + date.getSeconds());
+        str = str.replace(/s|S/g, date.getSeconds());
+        return str
+    },
+    /**
+     * 将时间日期字符串转换成时间戳得到的值类似于new Date().getTime
+     * @param 时间字符串（如：“2015-12-30 12:20:30”）
+     * @returns {number}
+     */
+    parse: function (timeStr) {
+        let date = new Date();
+        date.setFullYear(timeStr.substring(0, 4));
+        date.setMonth(timeStr.substring(5, 7) - 1);
+        date.setDate(timeStr.substring(8, 10));
+        date.setHours(timeStr.substring(11, 13));
+        date.setMinutes(timeStr.substring(14, 16));
+        date.setSeconds(timeStr.substring(17, 19));
+        return Date.parse(date);
+    }
+};
+
+
+/**
+ * 全选和反全选
+ */
+$.fn.checkAll = function () {
+    $(this).click(function () {
+        let _table = $(this).attr("data-table");
+        if ($(this).is(":checked")) {
+            $("#" + _table + " input:checkbox").each(function (index, element) {
+                this.checked = true;
+            });
+            $(".checkAll").each(function (index, ele) {
+                if ($(ele).attr("data-table") == _table) {
+                    ele.checked = true;
+                }
+            });
+        } else {
+            $("#" + _table + " input:checkbox").each(function (index, element) {
+                this.checked = false;
+            });
+            $(".checkAll").each(function (index, ele) {
+                if ($(ele).attr("data-table") == _table) {
+                    ele.checked = false;
+                }
+            });
+        }
+    });
+};
+
